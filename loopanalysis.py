c7cd1835bfc97b2de43a7f8f9b3f8a2736b90214
kavj <kjkamgar@gmail.com>
2021-03-16 18:49:28 -0700

    some cleanup, move unused expression tooling to utils

--- loopanalysis.py
+++ loopanalysis.py
@@ -1,204 +1,223 @@
-from functools import singledispatchmethod, cached_property
-
-import ir
-from visitor import VisitorBase, walk_assignments
-
-
-class LoopHeaderChecker:
+class ext_subscript:
     """
-    A general data structure for loop header analysis.
+    stub for an extended subscript
+    this is implementation only
+    it's used to record all indices
+    """
+    pass
 
-    This can process basically any for loop header.
-    It maintains a serialized view of the unpacking without
-    any assumptions that this unpacks correctly or that 
-    assignments are independent.
 
-    The simple and typical case can be specialized further.
+def collect_function_exits(func):
+    # Find any places a function may need to exit.
+    # This will eventually need to expand to include array allocation sites
+    pass
+
+
+def get_index_name(view):
+    """
+    Construct a name appropriate for iterating over this array dimension and stride pattern.
+    This can be further versioned in cases of conflicts.
+
+    It should be array name with _i, _j, _k, etc appended, starting from i.
+    In cases of conflicts, these can be versioned, so we might have _i0, _j0, _k0.
+
+    This is readable enough and doesn't greatly extend name lengths.
 
     """
-
-    def __init__(self, header: ir.ForLoop):
-        self.serialized = [*walk_assignments(header)]
-
-    @cached_property
-    def iterables(self):
-        return set(v for (_, v) in self.serialized)
-
-    @cached_property
-    def targets(self):
-        return set(v for (v, _) in self.serialized)
-
-    @cached_property
-    def unpacking_errors(self):
-        return {target: value for (target, value) in self.serialized if isinstance(target, ir.Tuple)}
-
-    @cached_property
-    def tuple_valued_targets(self):
-        return {target: value for (target, value) in self.serialized if isinstance(value, ir.Zip)}
-
-    @cached_property
-    def complex_assignments(self):
-        return {target: value for (target, value) in self.serialized if not isinstance(target, ir.NameRef)}
-
-    @cached_property
-    def is_well_formed_header(self):
-        if self.unpacking_errors:
-            # in pure python, this would either be an unpacking error or
-            # unpacking a named iterator that outputs a sequence of tuples.
-            return False
-        elif self.complex_assignments:
-            return False
-        elif self.tuple_valued_targets:
-            return False
-        return True
-
-    @cached_property
-    def is_range_loop(self):
-        if len(self.serialized) != 1:
-            return False
-        _, v = self.serialized[0]
-        return isinstance(v, ir.Counter)
-
-    @property
-    def as_target_map(self):
-        """
-        Get final syntactical mapping, ignoring any intermediate dependencies.
-
-        """
-
-        return {target: iterable for (target, iterable) in self.serialized}
+    pass
 
 
-class LoopDesc:
+def find_augmented(entry):
     """
-    This is the start of a high level loop descriptor.
-
-    It is intended to expose structural information.
-
-    It has to track non-trivial divergent statements (anything beyond if else),
-    nesting,
+    Find anything that is clobbered by name assignment or subscripted write, starting from entry.
 
     """
 
-    def __init__(self, is_for_loop):
-        self.is_for_loop = is_for_loop
-        self.header_map = {}
-        self.body_assigns = set()
-        self.nested_loops = []
-        self.divergent_stmts = []
-
-    def post_order_nested(self):
-        for n in self.nested_loops:
-            yield from n.post_order_nested()
-        yield self
-
-    @property
-    def loop_counters(self):
-        return {t: v for (t, v) in self.header_map.items() if v.is_counter}
-
-    @property
-    def is_while_loop(self):
-        return not self.is_for_loop
-
-    @property
-    def is_range_loop(self):
-        raise NotImplementedError
-
-    @property
-    def nested_count(self):
-        return len(self.nested_loops)
-
-    @property
-    def is_innermost_loop(self):
-        return len(self.nested_loops) == 0
-
-    @property
-    def nest_depth(self):
-        if self.is_innermost_loop:
-            return 0
-        return 1 + max(loop.nest_depth for loop in self.nested_loops)
-
-    @property
-    def may_exit_from_body(self):
-        contains_break_or_return = (self.divergent_stmts
-                                    and any(not isinstance(stmt, ir.Continue) for stmt in self.divergent_stmts))
-        return contains_break_or_return
+    pass
 
 
-class LoopNestBuilder(VisitorBase):
+def find_unpacking_errors(header):
+    # returns non-name reference targets, tuple to name assignments, attempted unpacking of name references
+    pass
+
+
+def get_header_targets(header):
     """
-    This checks loop divergence, loop counters, nesting structure, 
-    and what targets are clobbered in the body of each loop.
+    Find anything assigned by the iterator field of a loop entry header, ignoring while headers.
+
+    """
+    pass
+
+
+def get_named_header_iterables(header):
+    """
+    Get any named array references that are iterated over here.
+
+    """
+    pass
+
+
+def is_divergent_loop(header):
+    """
+    Check whether the current loop is divergent.
+    A divergent loop has a break statement at this level or a return statement at this level
+    or within any nested loop.
+    """
+
+    pass
+
+
+def contains_early_return(header):
+    """
+    Checks whether the loop body or any nested construct contains a return statement.
+    This could be expanded later to check for possible exits due to array allocation.
+    """
+
+    pass
+
+
+def find_varying_statements(entry):
+    """
+    Find statements that may vary in their output across a packet.
 
     """
 
-    def __init__(self):
-        self.loops = []
-        self.completed = None
+    pass
 
-    def __call__(self, node):
-        if self.loops:
-            raise RecursionError("LoopExitCollector cannot be called recursively")
-        elif not node.is_loop_entry:
-            raise TypeError
-        self.completed = []
-        self.visit(node)
-        assert (not self.loops)
-        completed = self.completed
-        self.completed = None
-        return completed
 
-    def build_loop_descriptor(self, node):
-        if isinstance(node, ir.ForLoop):
-            # counters = {target: value for (target, value) in targetmap.items() if value.is_counter}
-            loop = LoopDesc(True)
-            for target, value in walk_assignments(node):
-                loop.header_map[target] = value
-        else:
-            loop = LoopDesc(False)
-        if self.loops:
-            self.loops[-1].nested.append(node)
-        self.loops.append(loop)
-        self.visit(node.body)
-        loop = self.loops.pop()
-        assert (loop.header is node)
-        # only directly track top level
-        if not self.loops:
-            self.completed.append(loop)
+def get_directly_nested_loops(header):
+    """
+    Return any loop headers ordered by appearance, which appear within this loop body,
+    excluding further nesting levels.
+    """
+    pass
 
-    @singledispatchmethod
-    def visit(self, node):
-        return super().visit(node)
 
-    @visit.register
-    def _(self, node: ir.Assign):
-        for target, value in walk_assignments(node):
-            if not target.is_subscript:
-                self.loops[-1].body_assigns.add(target)
+def post_order_loops(header):
+    """
+    Returns a sequence of loop headers for the nest enclosed by header, in post order.
+    This could be a generator if we can guarantee a lack of modifications.
+    """
 
-    @visit.register
-    def _(self, node: ir.CascadeAssign):
-        for target in node.targets:
-            if not target.is_subscript:
-                self.loops[-1].body_assigns.add(target)
+    pass
 
-    @visit.register
-    def _(self, node: ir.Return):
-        if self.loops:
-            self.loops[-1].divergent_stmts.append(node)
 
-    @visit.register
-    def _(self, node: ir.Break):
-        self.loops[-1].divergent_stmts.append(node)
+def is_range_loop(header):
+    """
+    Determine if this is an IR representation of a range based for loop.
+    This determines how escape analysis is applied.
 
-    @visit.register
-    def _(self, node: ir.Continue):
-        self.loops[-1].divergent_stmts.append(node)
+    If the target value assigned by a range loop may escape the body of the loop where it's defined,
+    it cannot be used as a loop counter, since the value will still increment on loop exit.
 
-    @visit.register
-    def _(self, node: ir.WhileLoop):
-        self.build_loop_descriptor(node)
+    """
 
-    @visit.register
-    def _(self, node: ir.ForLoop):
-        self.build_loop_descriptor(node)
+    pass
+
+
+def find_loopnest_conflicts(assign, entry):
+    """
+    Find statements, which conflict with the current assignment, reachable from
+    the provided entry point.
+
+    This is used to check for cases where a variable name is overwritten or it reads
+    from an array that is modified at some point.
+
+    This is used in conjunction with escape analysis to determine how far forward we can move an iterated
+    assignment from an iterable, which may vary by packet lane.
+
+    This is used to avoid widening scalars, which vary by packet lane and are retrieved in outer loops
+    to short static arrays. In trivial cases, this allows us to privatize the scalars and leave the task of widening
+    to the back end compiler.
+
+    """
+    pass
+
+
+def find_loop_escapes(entry):
+    """
+    Find any variable reachable from entry that escapes the loop containing its first definition,
+    which is visible from entry
+
+    """
+    pass
+
+
+# write iterator transforms here and split later.
+# This way we have analysis utility stubs in full view.
+
+
+def may_have_negative_index(sub):
+    """
+    It's not yet clear what info will be needed here, even though we're only focusing on simple cases.
+
+    Find cases where we cannot guarantee that the reaching value of an index is strictly positive.
+
+    These are mostly cases where slicing contains subtraction or addition of possibly negative values.
+    It may be worth warning. I haven't yet decided on exact default behavior. Wrapping doesn't mix with simd.
+
+    """
+    pass
+
+
+def collect_iterator_strides(it):
+    """
+    Map iterables to strides.
+    This helps with reduction to simpler range loops.
+    It's worth tracking cases of dead variables wrapped in enumerate to constrain range, as we can preserve
+    the constraint without factoring this into the eventual range stride.
+
+    """
+    # Checks whether 1 or more iterables in an iterator nest uses a non-unit stride
+    pass
+
+
+def collect_iterator_offsets(it):
+    """
+    Map iterables to their starting offsets, with respect to their current view.
+    This is used to determine if offsets can be folded into range.start.
+
+    """
+
+    pass
+
+
+def get_iterable_length(it):
+    """
+    This is used to define a canonical iterable length expression for an iterable nest.
+    Since it's deterministic, we can check whether such an expression is already bound to some variable
+    name, using its hash.
+
+    """
+    pass
+
+
+def range_convert_loop(header, escapes):
+    """
+    Convert from an arbitrary iterator representation to a range loop, moving any assignments to
+    loop body subscripts.
+
+    """
+
+    pass
+
+
+def find_implem_name_conflicts(ctx):
+    """
+    This doesn't belong here, but it checks for naming conflicts on array parameters.
+
+    Internally I'm settling on conventions for things like index naming, based on what they index.
+
+
+    """
+
+    pass
+
+
+def flatten_index_expr(sub):
+    """
+    Turn a subscripted expression, which may dereference a strided or sliced array into an expression depicting
+    a flat index offset.
+
+    """
+    pass
